0570e3fc0ecb776abc7c179198b2f919d7748fe0
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-09-22 16:03:54 -0500

    add proyects

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,62 @@
 <script>
-  
+  import { Router, Route } from "svelte-routing";
+  import Navbar from "./Components/Navbar.svelte";
+  import Index from "./Pages/Index.svelte";
+  import Proyects from "./Pages/Proyects.svelte";
+  let routes = [
+    {
+      path: "/",
+      name: "Inicio",
+      component: Index,
+    },
+    {
+      path: "/proyectos",
+      name: "Proyectos",
+      component: Proyects,
+    },
+  ];
 </script>
 
 <style global>
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@100;200;300;400;500;600;700;800;900&display=swap");
   /* purgecss start ignore */
   @tailwind base;
   @tailwind components;
   /* purgecss end ignore */
   @tailwind utilities;
   /* podemos junstar clases para crear componentes */
-.btn {
-    @apply px-4 py-2 rounded font-monse;
-}
+  .btn {
+    @apply px-4 py-2 my-2 text-lg transition duration-700 ease-out delay-150 border-none rounded text-white;
+  }
 
-.btn-blue {
-    @apply text-white bg-blue-500;
-}
+  .btn-black {
+    @apply bg-blankHunt-500;
+  }
 
-.btn-blue:hover {
-    @apply bg-blue-700;
-}
+  .btn-black:hover {
+    @apply bg-greenHunt-500;
+  }
+  .btn-black:focus {
+    @apply outline-none bg-greenHunt-500 shadow-outline;
+  }
 
-a:hover {
+  a:hover {
     text-decoration: none;
-}
-a {
+  }
+  a {
     text-decoration: none;
-}
-a:visited{
+  }
+  a:visited {
     color: white;
-}
+  }
 </style>
 
-<main class="text-center font-monse">
-  <h1 class="text-6xl text-red-800">Hello </h1>
-  <p class="p-2">
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-  <p class="text-sm text-gray-600">TailwindCSS Integration</p>
-</main>
+<Router>
+  <div class="">
+    <Navbar {routes} />
+    {#each routes as { path, name, component }}
+      <Route {path} {component} />
+    {/each}
+  </div>
+</Router>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,55 @@
 const production = !process.env.ROLLUP_WATCH;
 module.exports = {
   purge: {
-    content: [
-      "./src/**/*.svelte",
-    ],
-    enabled: production // disable purge in dev
+    content: ["./src/**/*.svelte"],
+    enabled: production, // disable purge in dev
   },
   theme: {
     extend: {
-        colors: {
-            danger: '#ff5f40',
-            info: {
-                900: '#234e52',
-                800: '#285e61',
-            },
+      colors: {
+        danger: "#ff5f40",
+        info: {
+          900: "#234e52",
+          800: "#285e61",
         },
-        width: {
-            '1/8': '12.5%',
-            '2/8': '25%',
-            '3/8': '37.5%',
-            '4/8': '50%',
-            '5/8': '62.5%',
-            '6/8': '75%',
-            '7/8': '87.5%',
+        blankHunt: {
+          600: "#222831",
+          500: "#393e46",
         },
-        minWidth: {
-            0: '0',
-            '1/4': '25%',
-            '1/2': '50%',
-            '3/4': '75%',
-            full: '100%',
+        greenHunt: {
+          500: "#29a19c",
+          300: "#a3f7bf",
         },
+      },
+      width: {
+        "1/8": "12.5%",
+        "2/8": "25%",
+        "3/8": "37.5%",
+        "4/8": "50%",
+        "5/8": "62.5%",
+        "6/8": "75%",
+        "7/8": "87.5%",
+      },
+      minWidth: {
+        0: "0",
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%",
+        full: "100%",
+      },
     },
     fontFamily: {
-        monse: ['Montserrat'],
+      monse: ["Grenze Gotisch"],
+      montserrat: ["Montserrat"],
     },
-},
-variants: {},
-plugins: [require('@tailwindcss/custom-forms')],
-  future: { // for tailwind 2.0 compat
+  },
+  variants: {
+    borderStyle: ["responsive", "hover", "focus", "active"],
+    backgroundColor: ["responsive", "hover", "focus", "active", "group-hover"],
+  },
+  plugins: [require("@tailwindcss/custom-forms")],
+  future: {
+    // for tailwind 2.0 compat
     purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
